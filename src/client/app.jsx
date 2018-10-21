566dd19838f34e0ba8d84fbdc5b0fd846d2835c7
Trever Hillis <revertcreations@gmail.com>
2018-10-21 12:15:12 -0600

    start of redux client side

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,7 +1,15 @@
 // @flow
 
 import React from 'react'
+import HelloButton from './container/hello-button'
+import Message from './container/message'
+import { APP_NAME } from '../shared/config'
 
-const App = () => <h1 style={{ textAlign: 'center' }}>Hello React, from Trever!</h1>
+const App = () =>
+  <div>
+    <h1>{APP_NAME}</h1>
+    <Message />
+    <HelloButton />
+  </div>
 
 export default App
